89f73e5492f1dc2622375f23a7fbdb27a3777547
Abdelrahman <Bdalrhmnalshnawy08@gmail.com>
2026-07-25 11:23:19 +0300

    Remove node_modules and .env

--- utils/ApiError.cjs
+++ utils/ApiError.cjs
@@ -1,27 +1,4 @@
-// class ApiError extends Error {
-//     constructor(message, statusCode) {
-//         // استدعاء الـ constructor الخاص بـ Error
-//         super(message);
 
-//         // حفظ الـ Status Code
-//         this.statusCode = statusCode;
-
-//         // تحديد نوع الخطأ
-//         // لو 4xx يبقى Fail
-//         // لو 5xx يبقى Error
-//         this.status = `${statusCode}`.startsWith("4")
-//             ? "fail"
-//             : "error";
-
-//         // الخطأ متوقع (Operational Error)
-//         this.isOperational = true;
-
-//         // الاحتفاظ بمكان حدوث الخطأ
-//         Error.captureStackTrace(this, this.constructor);
-//     }
-// }
-
-// module.exports = ApiError;
 class ApiError extends Error {
 
     constructor(message, statuscode) {
