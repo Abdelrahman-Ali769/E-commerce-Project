9c2efbc168ea11cf94f7d1f8222e34be34733cbf
Abdulrahman Ali <abdulrahmanelshenawy2@gmail.com>
2026-08-11 17:58:15 +0300

    add product database seeder

--- utils/dummyData/seeder.js
+++ utils/dummyData/seeder.js
@@ -1,12 +1,16 @@
 const fs = require("fs");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const ProductModel = require("../Models/ProductSchema.cjs");
 
-dotenv.config();
+const ProductModel = require("../../Models/ProductSchema.cjs");
+
+dotenv.config({
+  path: `${__dirname}/../../.env`,
+});
 
 mongoose.set("strictQuery", false);
 
+// Connect to Database
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Database Connected"))
@@ -15,14 +19,18 @@ mongoose
     process.exit(1);
   });
 
+// Read Products JSON File
 const products = JSON.parse(
-  fs.readFileSync("./utils/dummyData/products.json", "utf-8")
+  fs.readFileSync(`${__dirname}/products.json`, "utf-8")
 );
 
+// Insert Data
 const InsertData = async () => {
   try {
     await ProductModel.create(products);
+
     console.log("Data Inserted Successfully.");
+
     process.exit();
   } catch (error) {
     console.error(error);
@@ -30,16 +38,23 @@ const InsertData = async () => {
   }
 };
 
+// Delete Data
 const DeleteData = async () => {
   try {
     await ProductModel.deleteMany();
+
     console.log("Data Deleted Successfully.");
+
     process.exit();
   } catch (error) {
     console.error(error);
     process.exit(1);
   }
 };
+
+// Run Seeder
+// node seeder.js -i => Insert Data
+// node seeder.js -d => Delete Data
 
 if (process.argv[2] === "-i") {
   InsertData();
