84741225821d9503117c70757a940d1df5543def
Abdulrahman Ali <abdulrahmanelshenawy2@gmail.com>
2026-09-02 20:12:51 +0300

    fix: validate image upload by file extension

--- Controllers/CategoryController.cjs
+++ Controllers/CategoryController.cjs
@@ -1,21 +1,35 @@
 const CategoryModel = require("../Models/CategorySchema.cjs");
+const ApiError = require("../utils/ApiError.cjs");
 const factoryHandler = require("./FactoyHandlers.cjs");
 const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 
-const MulterStorage =multer.diskStorage({
-    destination: function(req,file,cb){
-        cb(null,"uploads/categories")
+const MulterStorage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads/categories")
     },
-    filename :function(req,file,cb){
+    filename: function (req, file, cb) {
         console.log(req.file)
-        const ext =file.mimetype.split('/')[1]
-        const filename =`catgory-${uuidv4()}-${Date.now()}.${ext}`
+        const ext = file.mimetype.split('/')[1]
+        const filename = `category-${uuidv4()}-${Date.now()}.${ext}`
         cb(null, filename);
     }
 })
+const MulterFilter = function (req, file, cb) {
+    // تحقق من الامتداد بس (extension)
+    const allowedExtensions = ['jpg', 'jpeg', 'png', 'gif', 'webp'];
+    const fileExtension = file.originalname.split('.').pop().toLowerCase();
+    
+    if (allowedExtensions.includes(fileExtension)) {
+        cb(null, true);
+    } else {
+        cb(new ApiError("Only Images Allowed", 400), false);
+    }
+};
+
 const upload = multer({
-    storage : MulterStorage
+    storage: MulterStorage,
+    fileFilter: MulterFilter
 })
 exports.uploadCategoryImage = upload.single("image")
 
