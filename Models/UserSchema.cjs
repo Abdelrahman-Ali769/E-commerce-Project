61fed2b7f154dbd4f5cae2a3a66b441e4c506411
Abdulrahman Ali <abdulrahmanelshenawy2@gmail.com>
2026-09-23 11:50:28 +0300

    feat: create user schema with authentication fields

--- Models/UserSchema.cjs
+++ Models/UserSchema.cjs
@@ -1,31 +1,49 @@
-const mongoose  = require('mongoose')
-const UserSchema = new mongoose.Schema({
-    name : {
-        type  :String,
-        trim : true ,
-        required :[true,"Name is Required"] 
-    } ,
-    slug : {
-        type :String ,
-        lowercase: true
-    } ,
-    email : {
-        type :String ,
-        required : [true ,"Email requred"] ,
-        lowercase :true ,
-        unique : true
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: [true, 'Name is required'],
+        },
+
+        slug: {
+            type: String,
+            lowercase: true,
+        },
+
+        email: {
+            type: String,
+            required: [true, 'Email is required'],
+            lowercase: true,
+            unique: true,
+            trim: true,
+        },
+
+        phone: {
+            type: String,
+        },
+
+        ProfileImage: {
+            type: String,
+        },
+
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+            minlength: [6, 'Password is too short'],
+        },
+
+        role: {
+            type: String,
+            enum: ['user', 'admin'],
+            default: 'user',
+        },
     },
-    phone  :String ,
-    ProfileImage :String,
-    password : {
-        type :String ,
-        required :[true , "Required"] ,
-        minlength   :[6,"Too short"] 
-    },
-    role :{
-        type: String ,
-        enum :['user','admin'],
-        default : 'user'
+    {
+        timestamps: true,
     }
-},{timestamps :true})
-module.exports =mongoose.model('UserSchema',UserSchema)
+);
+
+module.exports = mongoose.model('User', UserSchema);
